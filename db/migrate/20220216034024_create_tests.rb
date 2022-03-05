e79ac20abf51bc8ede57ad933f5f1464241e2b52
nana0519 <95403153+nana0519@users.noreply.github.com>
2022-03-05 02:53:50 +0000

    gitignoreの練習

--- db/migrate/20220216034024_create_tests.rb
+++ db/migrate/20220216034024_create_tests.rb
@@ -1,7 +1,7 @@
 class CreateTests < ActiveRecord::Migration[6.1]
   def change
     create_table :tests do |t|
-      t.string:name
+      t.string:test_name
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2022_02_16_034024) do
 
   create_table "tests", force: :cascade do |t|
-    t.string "name"
+    t.string "test_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
